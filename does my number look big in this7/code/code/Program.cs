30ba47d4341d682f204385d394e594756f141f92
melihovodina <melihoxa@gmail.com>
2023-09-08 16:26:06 +0300

    Initial commit

---
+++ does my number look big in this7/code/code/Program.cs
@@ -0,0 +1,34 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+Console.WriteLine(Kata.Narcissistic(153)); //On codewars this line isn't needed
+public class Kata
+{
+    public static bool Narcissistic(int value)
+    {
+        int a = value;
+        int[] digits = new int[value.ToString().Length];
+        int index = digits.Length - 1;
+        while (value > 0)
+        {
+            digits[index] = value % 10;
+            value /= 10;
+            index--;
+        }
+        double sum = 0;
+        for(int i = 0; i < digits.Length; i++)
+        {
+            int num = 1;
+            int pow = digits.Length;
+            while (pow > 0)
+            {
+                num *= digits[i];
+                pow--;
+            }
+            sum += num;
+        }
+        if(sum == a)
+            return true;
+        else 
+            return false;
+    }
+}
---
+++ multiples of 3 or 5/multiples of 3 or 5/multiples of 3 or 5/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine(Kata.Solution(10)); //On codewars this line isn't needed
+public static class Kata
+{
+    public static int Solution(int value)
+    {
+        int a = 0, sum = 0;
+        for (int i = 0; i < value; i++)
+        {
+            if (a % 3 == 0)
+                sum += a;
+            else if (a % 5 == 0)
+                sum += a;
+            a++;
+        }
+        return sum;
+    }
+}
