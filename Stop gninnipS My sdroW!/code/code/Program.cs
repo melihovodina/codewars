af10ec57f75d5497441da779ea2a8fdd296865c3
melihovodina <melihoxa@gmail.com>
2023-09-13 12:33:45 +0300

    done 1 ex

---
+++ Stop gninnipS My sdroW!/code/code/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System;
+Console.WriteLine(Kata.SpinWords("Hey fellow warriors"));
+public class Kata
+{
+    public static string SpinWords(string sentence)
+    {
+        string[] words = sentence.Split(' ');
+        char[] a;
+        for (int i = 0; i < words.Length; i++)
+        {
+            if (words[i].Length >= 5)
+            {
+                a = words[i].ToCharArray();
+                Array.Reverse(a);
+                words[i] = "";
+                for(int j = 0; j < a.Length; j++)
+                {
+                    words[i] += a[j];
+                }
+            }   
+
+        }
+        string result = string.Join(" ", words);
+        return result;
+    }
+}
